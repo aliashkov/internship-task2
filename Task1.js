9f0670fe2a1e45bd263c03cba9d00afdb6689b0d
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-24 11:14:18 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -1,8 +1,44 @@
-function currentSums(simpleNumbers){
-    let changedArray = [];
-    simpleNumbers.reduce((sum, current, index) => changedArray[index] = sum + current, 0);
-    return changedArray;
+function compress(originalString) {
+    let compressedString = '';
+
+    if (originalString.length === 0) {
+        console.log('Error. This string must contain at least 1 symbol');
+        return;
+    }
+
+    let amountRepeatings = 0;
+
+    for (let i = 0; i < originalString.length; i++) {
+        amountRepeatings++;
+        if (originalString[i] != originalString[i + 1]) {
+            compressedString += originalString[i] + amountRepeatings;
+            amountRepeatings = 0;
+        }
+    }
+
+    return compressedString;
 }
 
-let result4 = currentSums([2,3,5,7,11,13,17,19]);
-console.log(result4);
+
+
+
+function uncompress(originalString) {
+    let uncompressedString = '';
+    
+    if (originalString.length === 0) {
+        console.log('Error. This string must contain at least 1 symbol');
+        return;
+    }
+
+    for (let i = 0; i < originalString.length; i += 2) 
+        uncompressedString += originalString[i].repeat(originalString[i + 1]).split(',');
+
+    return uncompressedString;
+}
+
+let output;
+console.log(output = compress('aaaa')); //a4
+console.log(output = compress('aaaabbc')); //a4b2c1
+console.log(output = compress('aaaabbcaabb')); //a4b2c1a2b2
+console.log(output = uncompress('a4b2')); //aaaaabb
+console.log(output = uncompress('a2b1c3')); //aabccc
